5ffebc93b4ad195c7742dfc4429f3a49ae9014b0
davidy838 <davidyang838@icloud.com>
2023-12-29 13:26:03 -0700

    new

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,8 +1,30 @@
+import ChatHeader from './ChatHeader';
+import MatchesDisplay from './MatchesDisplay';
+import ChatDisplay from './ChatDisplay';
+
 const ChatContainer = () => {
     return (
-    <div> 
-        Chat Container
-    </div>);
+
+        <div className="chat-container">
+            <ChatHeader /> 
+
+            <div>
+
+                <button className="option">Links</button>
+                <button className="option">Chats</button>
+            </div>
+
+            <MatchesDisplay/>
+
+            <ChatDisplay/>
+
+
+
+
+
+        </div>
+
+    );
 
 }
 export default ChatContainer;
