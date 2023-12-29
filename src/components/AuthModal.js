2072e5a34703b0d232e34ad3c0672268f3723146
davidy838 <davidyang838@icloud.com>
2023-12-28 21:32:42 -0700

    updates

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 
-const AuthModal = ({ setShowModal }) => {
+const AuthModal = ({ setShowModal, isSignUp}) => {
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
     const [confirmPassword, setConfirmPassword] = useState(null);
@@ -10,13 +10,19 @@ const AuthModal = ({ setShowModal }) => {
     console.log(email, password, confirmPassword)
 
     const handleClick = () => {
-        setShowModal(false)    
+        setShowModal(false);   
     }
-    const isSignup = true;
     const handleSubmit = (e) => { 
-        e.preventDefault();
-        console.log("submitted")
+        e.preventDefault()
+        try {
+                if (isSignUp && (password !== confirmPassword)) {
+                    throw new Error("Passwords do not match")
+                }
+                console.log('make a post request to database')
 
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
@@ -25,7 +31,7 @@ const AuthModal = ({ setShowModal }) => {
 
     <div className="auth-modal">
         <div className="close-icon" onClick={handleClick}>ⓧ</div>
-        <h2> {isSignup ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+        <h2> {isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
         <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
         <form onSubmit={handleSubmit}>
             <input
@@ -46,19 +52,21 @@ const AuthModal = ({ setShowModal }) => {
                 onChange={(e) => setPassword(e.target.value)}
 
             />
-            <input
-                type='password-check'
+            {isSignUp && <input
+                type='password'
                 id='password-check'
                 name='password-check'
                 placeholder='confirm password'
                 required={true}
                 onChange={(e) => setConfirmPassword(e.target.value)}
 
-            />
+            />}
             <input className="secondary-button" type="submit"/>
             <p>{error}</p>
 
         </form>
+        <hr/>
+        <h2>GET THE APP</h2>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,19 @@
 import logo from '../images/main.jpeg';
 import logo2 from '../images/main2.jpeg';
 
-const Nav = ({ minimal, authToken, setShowModal, showModal })=> {
+const Nav = ({ minimal, setShowModal, showModal, setIsSignUp})=> {
     const handleClick = () => {
         setShowModal(true);
+        setIsSignUp(false);
     }
+
+    const authToken = true;
     return (
         <nav>
             <div className="logo-container">
                 <img className="logo" src={minimal? logo : logo2 } alt = "logo"/>
             </div>
-            {!authToken  && 
+            {!authToken && !minimal && 
             <button 
             className="nav-button" 
             onClick={handleClick}
