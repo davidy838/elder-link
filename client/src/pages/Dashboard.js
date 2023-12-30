4ff9566bb467743959bddd649411b12a6fb49103
davidy838 <davidyang838@icloud.com>
2023-12-30 12:14:03 -0700

    changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -10,6 +10,8 @@ const Dashboard = () => {
   const [cookies, setCookie, removeCookie] = useCookies(['user'])
   const userId = cookies.UserId
   const [genderedUsers, setGenderedUsers] = useState(null)
+  const [lastDirection, setLastDirection] = useState()
+
   const getUser = async () => {
     try {
         console.log('userId', userId)
@@ -21,7 +23,7 @@ const Dashboard = () => {
         console.log(error)
         }
     }
-   const getGendereUsers = async() => {
+   const getGenderedUsers = async() => {
        try {
         const response = await axios.get('http://localhost:8000/gendered-users', {
            params: { gender: user?.gender_interest }
@@ -34,48 +36,44 @@ const Dashboard = () => {
 
     useEffect(() => {
         getUser()
-        getGendereUsers
+        getGenderedUsers()
     }, [user, genderedUsers]);
 
 
 
+    const updateMatches = async(matchedUserId) => {
+      try {
+        await axios.put('http://localhost:8000/addmatch', {
+          userId, matchedUserId 
+        })
+        getUser()
+      }
+      catch (error) {
+        console.log(error)
+      }
+
+
+    }
 
 
 
 
 
-    const characters = [
-        {
-          name: 'Richard Hendricks',
-          url: 'https://i.imgur.com/oPj4A8u.jpeg'
-        },
-        {
-          name: 'Erlich Bachman',
-          url: 'https://i.imgur.com/oPj4A8u.jpeg'
-        },
-        {
-          name: 'Monica Hall',
-          url: 'https://i.imgur.com/oPj4A8u.jpeg'
-        },
-        {
-          name: 'Jared Dunn',
-          url: 'https://i.imgur.com/oPj4A8u.jpeg'
-        },
-        {
-          name: 'Dinesh Chugtai',
-          url: 'https://i.imgur.com/oPj4A8u.jpeg'
-        }
-      ]
-    const [lastDirection, setLastDirection] = useState()
   
-    const swiped = (direction, nameToDelete) => {
-      console.log('removing: ' + nameToDelete)
+    const swiped = (direction, swipedUserId) => {
+      if (direction === 'right'){
+        updatedMatches(swipedUserId)
+      }
+
       setLastDirection(direction)
     }
+  
   
     const outOfFrame = (name) => {
       console.log(name + ' left the screen!')
     }
+
+
     return (
       <>
       {user && 
@@ -83,14 +81,14 @@ const Dashboard = () => {
             <ChatContainer user={user}/>
             <div className="swipe-container">
                 <div className="card-container">
-                    {characters.map((character) =>
+                    {genderedUsers?.map((genderedUser) =>
                 <TinderCard 
                 className='swipe' 
-                key={character.name} 
-                onSwipe={(dir) => swiped(dir, character.name)} 
-                onCardLeftScreen={() => outOfFrame(character.name)}>
-                    <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
-                    <h3>{character.name}</h3>
+                key={genderedUser.first_name} 
+                onSwipe={(dir) => swiped(dir, genderedUser.user_id)} 
+                onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}>
+                    <div style={{ backgroundImage: 'url(' + genderedUser.url + ')' }} className='card'>
+                    <h3>{genderedUser.first_name}</h3>
                     </div>
                 </TinderCard>
                 )} 
