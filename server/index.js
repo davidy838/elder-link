f37773ada26dc51fbb6411e8f1a19c1ae831439f
davidy838 <davidyang838@icloud.com>
2023-12-30 23:35:47 -0700

    new

--- server/index.js
+++ server/index.js
@@ -279,6 +279,25 @@ app.get('/messages', async (req, res) => {
 })
 
 
+app.post('/message', async (req, res) => {
+    const client = new MongoClient(uri)
+    const message = req.body.message
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const messages = database.collection('messages')
+
+        const insertedMessage = await messages.insertOne(message)
+        res.send(insertedMessage)
+    }
+    finally {
+        await client.close()
+    }
+
+})
+
+
 
 
 
