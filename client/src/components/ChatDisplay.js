582a774c85b6b5b8100b8f743313319e99149a1d
davidy838 <davidyang838@icloud.com>
2023-12-30 23:36:08 -0700

    new

--- client/src/components/ChatDisplay.js
+++ client/src/components/ChatDisplay.js
@@ -9,8 +9,9 @@ const ChatDisplay = ( {user, clickedUser}) => {
     const userId = user?.user_id
     const clickedUserId = clickedUser?.user_id
     const [usersMessages, setUsersMessages] = useState(null)
+    const [clickedUsersMessages, setClickedUsersMessages] = useState(null)
 
-    const getUserMessages = async () => {
+    const getUsersMessages = async () => {
         try {
         const response = await axios.get('http://localhost:8000/messages', {
             params: { userId: userId, correspondingUserId: clickedUserId }
@@ -20,15 +21,54 @@ const ChatDisplay = ( {user, clickedUser}) => {
         console.log(error)
     }
     }
-    useEffect(() => { getUserMessages() }, [usersMessages])
+    const getClickedUsersMessages = async () => {
+        try {
+        const response = await axios.get('http://localhost:8000/messages', {
+            params: { userId: clickedUserId, correspondingUserId: userId}
+    })
+     setUsersMessages(response.data)}
+     catch(error) {
+        console.log(error)
+    }
+    }
+    
+    useEffect(() => { getUsersMessages()
+        getClickedUsersMessages()
+    }, [])
 
-    console.log(usersMessages)
+    const messages = []
+    usersMessages?.forEach(message => {
+        const formattedMessage = {}
+        formattedMessage['name'] = user?.first_name
+        formattedMessage['img'] = user?.url
+        formattedMessage['message'] = message.message
+        formattedMessage['timestamp'] = message.timestamp
+        messages.push(formattedMessage)
+    })
+    clickedUsersMessages?.forEach(message => {
+        const formattedMessage = {}
+        formattedMessage['name'] = clickedUser?.first_name
+        formattedMessage['img'] = clickedUser?.url
+        formattedMessage['message'] = message.message
+        formattedMessage['timestamp'] = message.timestamp
+        messages.push(formattedMessage)
+    })
+
+    const descendingOrderMessages = messages?.sort((a, b) => a.timestamp.localCompare(b.timestamp))
+
+
+
 
 
     return (
         <>
-        <Chat />
-        <ChatInput />
+        <Chat descendingOrderMessages={descendingOrderMessages}/>
+        <ChatInput 
+        user = {user}
+        clickedUser = {clickedUser}
+        getUsersMessages = {getUsersMessages}
+        getClickedUsersMessages = {getClickedUsersMessages}
+        />
         </>
     )
 }
